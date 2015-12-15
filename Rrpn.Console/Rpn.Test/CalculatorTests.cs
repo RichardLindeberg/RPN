b9550da59769f0d912d4f10a3a1e771e9c3095a2
RichardLindeberg <richard.lindeberg@gmail.com>
2015-12-15 21:16:27 +0100

    Ok, maybe I need to learn git..

---
+++ Rrpn.Console/Rpn.Test/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rpn.Core;
+
+namespace Rpn.Test
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        public void FiveAndFivePlussIsTen()
+        {
+            var expected = 10M;
+            var calculator = new Calculator();
+            calculator.AddNumber(5);
+            calculator.AddNumber(5);
+            calculator.AddOperator(Operator.Plus);
+            var actaul = calculator.Calculate();
+            Assert.AreEqual(actaul, expected);
+        }
+    }
+}
